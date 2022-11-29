19e8c03abce5396a52e1430f919b7e6ed5ed4920
m-riss <95452643+m-riss@users.noreply.github.com>
2022-11-28 19:51:42 -0500

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,7 +1,12 @@
-class a {
+#include <iostream>
+using namespace std;
+
+int main() {
+ class a {
 public:
 float a = 3.1415927F;
 float b = 57.295776F;
 float c = 0.017453292F;
 float d = 1.0E-6F;
+}
 }
